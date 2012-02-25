d4977168df8895c6934833f4cf20c171bc9f63c5
Kuba Marek <blue.cube@seznam.cz>
2012-02-25 21:17:59 +0100

    Fixed test code.

--- test.py
+++ test.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
-import db_net_udp
-import db_net_registers
+import db_net.udp
+import db_net.registers
 
 from hexdump import *
 
@@ -9,22 +9,22 @@ for data in open('packets.txt', 'r'):
 
     print('*' * 80)
 
-    p = db_net_udp.Packet.from_bytes(data)#, 48414)
-    if p.mode == db_net_udp.Packet.INVALID_STATION_KEY:
+    p = db_net.udp.Packet.from_bytes(data)#, 48414)
+    if p.mode == db_net.udp.Packet.INVALID_STATION_KEY:
         print("Correcting station key (0x{:x})".format(p.station_key))
         continue
     
     print('{} -> {}'.format(p.dbnet_packet.sa, p.dbnet_packet.da))
 
     try:
-        rq = db_net_registers.ReadRequest.from_bytes(p.dbnet_packet.payload)
+        rq = db_net.registers.ReadRequest.from_bytes(p.dbnet_packet.payload)
     except:
         pass
     else:
         print(rq)
         continue
 
-    resp = db_net_registers.ReadResponse.from_bytes(p.dbnet_packet.payload, rq)
+    resp = db_net.registers.ReadResponse.from_bytes(p.dbnet_packet.payload, rq)
     try:
         pass
     except Exception as e:
