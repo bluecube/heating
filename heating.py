9093dc8a218e3f59d0bc7e1658b0fa7afb782fa3
Kuba Marek <blue.cube@seznam.cz>
2012-02-27 10:51:02 +0100

    More complete data in states and factory for creating them.

--- heating.py
+++ heating.py
@@ -2,19 +2,25 @@ import numpy
 import time
 import sys
 import itertools
+import copy
 
 class State:
     FILE_SEPARATOR = b'|'
 
     def __init__(self, prev_state,
-        room_temp, heating_on, heating_temp, heating_return_temp,
-        outside_temp, timestamp = None):
+        room_temp, heating_on,
+        heating_temp, return_temp,
+        outside_temp,
+        dhw_bottom_temp, buffer_temp,
+        timestamp = None):
 
         self.room_temp = room_temp
-        self.heating_on = heating_on
+        self.heating_on = numpy.array(heating_on, dtype=numpy.float)
         self.heating_temp = heating_temp
-        self.heating_return_temp = heating_return_temp
+        self.return_temp = return_temp
         self.outside_temp = outside_temp
+        self.dhw_bottom_temp = dhw_bottom_temp
+        self.buffer_temp = buffer_temp
 
         if timestamp is None:
             self.timestamp = time.time()
@@ -27,12 +33,8 @@ class State:
 
         self.d_temp = (room_temp - prev_state.room_temp) / (self.timestamp - prev_state.timestamp)
 
-    def save(self, f):
-        self._w_line(f, self.room_temp)
-        self._w_line(f, self.heating_on)
-        self._w_line(f, self.d_temp)
-        self._w_line(f, (self.heating_temp, self.heating_return_temp,
-            self.outside_temp, self.timestamp))
+    def is_complete(self):
+        return self.d_temp is not None
 
     @classmethod
     def _rd_line(cls, f, dtype):
@@ -47,18 +49,54 @@ class State:
         f.write(cls.FILE_SEPARATOR.join((str(x).encode('ascii') for x in line)))
         f.write(b'\n')
 
+    def save(self, f):
+        self._w_line(f, self.room_temp)
+        self._w_line(f, self.heating_on)
+        self._w_line(f, self.d_temp)
+        self._w_line(f, (self.heating_temp, self.return_temp,
+            self.outside_temp, self.dhw_bottom_temp, self.buffer_temp,
+            self.timestamp))
+
     @classmethod
     def load(cls, f):
-        self = cls(None, None, None, None, None)
+        self = cls(None, None, None, None, None, None, None, None)
 
         try:
             self.room_temp = cls._rd_line(f, numpy.float)
-            self.heating_on = cls._rd_line(f, numpy.bool)
+            self.heating_on = cls._rd_line(f, numpy.float)
             self.d_temp = cls._rd_line(f, numpy.float)
-            self.heating_temp, self.heating_return_temp, \
-                self.outside_temp, self.timestamp = cls._rd_line(f, numpy.float)
+            self.heating_temp, self.return_temp, \
+                self.outside_temp, self.timestamp, \
+                self.dhw_bottom_temp, self.buffer_temp = cls._rd_line(f, numpy.float)
         except StopIteration:
             return None
+
+        return self
+
+    @classmethod
+    def average(cls, states_iterable):
+        it = iter(states_iterable)
+
+        self = copy.deepcopy(next(it))
+        count = 1
+
+        for state in it:
+            self.room_temp += state.room_temp
+            self.heating_on += state.heating_on
+            self.heating_temp += state.heating_temp
+            self.return_temp += state.return_temp
+            self.outside_temp += state.outside_temp
+            self.dhw_bottom_temp += state.dhw_bottom_temp
+            self.buffer_temp += state.buffer_temp
+            count += 1
+            
+        self.room_temp /=count
+        self.heating_on /=count
+        self.heating_temp /=count
+        self.return_temp /=count
+        self.outside_temp /=count
+        self.dhw_bottom_temp /=count
+        self.buffer_temp /=count
 
         return self
 
@@ -72,7 +110,10 @@ class State:
             lines.append('{}: t = {:.2f}°C, heating = {}, dt/dtau = {}'.format(i, float(t), h, dt))
 
         lines.append('outside temp = {:.2f}°C, heating temp = {:.2f}°C -> {:.2f}°C'.format(
-            float(self.outside_temp), float(self.heating_temp), float(self.heating_return_temp)))
+            float(self.outside_temp), float(self.heating_temp), float(self.return_temp)))
+
+        lines.append('dhw temp = {:.2f}°C, buffer temp = {:.2f}°C'.format(
+            float(self.dhw_bottom_temp), float(self.buffer_temp)))
 
         return '\n'.join(lines)
 
---
+++ state_factory.py
@@ -0,0 +1,71 @@
+import heating
+
+import db_net.udp
+import db_net.registers
+
+import numpy
+
+class StateFactory:
+    """
+    Create states from dbnet registers.
+    This is specific to our house.
+    """
+    PASSWORD = 48414
+    ADDRESS = ('10.0.0.202', 59)
+    MY_STATION = 25
+    REMOTE_STATION = 4
+
+    ROOM_COUNT = 19
+
+    def __init__(self):
+        self.conn = db_net.udp.Client(
+            self.ADDRESS,
+            self.REMOTE_STATION,
+            self.PASSWORD,
+            dbnet_source_addr = self.MY_STATION)
+
+        self.temp_reg = db_net.registers.Register(self.conn, 4112, 'MF[25,1]', auto_update = True)
+        self.status_reg = db_net.registers.Register(self.conn, 4113, 'MI[1,5]', auto_update = True)
+            # There's no need to read the whole matrix when only part of it will be used.
+
+        self.heating_count = self.get_heating_count()
+
+        self.prevState = None
+
+    def get_heating_count(self):
+        ordering_reg = db_net.registers.Register(self.conn, 4082, 'MI[1,32]', auto_update = True)
+
+        for i, value in enumerate(ordering_reg.value[0,:]):
+            if value == 255:
+                return i
+
+        return 32
+
+    def get_state(self):
+        temps = self.temp_reg.value
+        status = self.status_reg.value
+
+        room_temps = temps[:self.ROOM_COUNT,0]
+        all_temps = temps[:,0]
+
+        bits = status[0, 3] | (status[0, 4] << 8)
+
+        heating_on = numpy.zeros(self.heating_count, dtype=numpy.bool)
+        i = 0
+        while bits > 0:
+            heating_on[i] = bool(bits & 0x01)
+            bits = bits >> 1
+            i = i + 1
+
+        state = heating.State(self.prevState,
+            room_temps, heating_on,
+            heating_temp = all_temps[24],
+            return_temp = all_temps[23],
+            outside_temp = all_temps[19],
+            dhw_bottom_temp = all_temps[21],
+            buffer_temp = all_temps[22])
+
+        self.prevState = state
+
+        return state
+
