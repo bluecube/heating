b0e110e0ca04cf5458d148138d819ed9342133ea
Kuba Marek <blue.cube@seznam.cz>
2012-03-29 18:34:02 +0200

    Recording prepared for plotting.

--- recording.py
+++ recording.py
@@ -3,7 +3,9 @@
 import time
 import gzip
 import sys
-import collections
+import itertools
+
+#import matplotlib.pyplot as plt
 
 import state_factory
 import heating
@@ -11,36 +13,48 @@ import heating
 INTERVAL = 60
 AVERAGE_COUNT = 10
 
+PLOT_LEN = 24 * 3600
+PLOT_COUNT = PLOT_LEN // INTERVAL
+
 sf = state_factory.StateFactory()
 
-display = collections.deque(maxlen = (24 * 3600) // INTERVAL)
 average = []
 
 print('running')
 
+f = gzip.open(sys.argv[1], 'ab')
+
+state = sf.get_state()
+print(state)
+
+#plot_values = numpy.zeroes((2 * PLOT_COUNT, len(state.values['room_temp'])))
+#plot_times = numpy.zeroes(2 * PLOT_COUNT)
+
 try:
-    with gzip.open(sys.argv[1], 'ab') as f:
+    while True:
+#    for i in itertools.cycle(range(PLOT_COUNT)):
+#        plot_values[i,:] = state.values['room_temp']
+#        plot_times[i] = state.timestamp
+#        plot_values[i + PLOT_COUNT,:] = state.values['room_temp']
+#        plot_times[i + PLOT_COUNT] = state.timestamp
+#        plt.plot_date(plot_times, plot_values)
+
+        time.sleep(INTERVAL)
 
         state = sf.get_state()
         print(state)
-        time.sleep(INTERVAL)
 
-        while True:
-            state = sf.get_state()
-            print(state)
+        average.append(state)
 
-            display.append(state)
-            average.append(state)
+        if len(average) == AVERAGE_COUNT:
+            averaged = heating.State.average(average)
 
-            if len(average) == AVERAGE_COUNT:
-                averaged = heating.State.average(average)
+            print("\nsaving:")
+            print(averaged)
 
-                print("\nsaving:")
-                print(averaged)
-
-                averaged.save(f)
-                average = []
-
-            time.sleep(INTERVAL)
+            averaged.save(f)
+            average = []
 except KeyboardInterrupt:
     print('interrupted')
+finally:
+    f.close()
